5fb6396062afd0305404c17b09022acc3ff41c89
Sebastian Valencia <maxdevtool@gmail.com>
2024-11-22 08:32:08 -0500

    feat 04: add pagination, update product, soft delete, new migrations

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,7 +1,8 @@
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
+import { PaginationDto } from 'src/common';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
@@ -19,19 +20,63 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     })
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async findAll(paginationDto: PaginationDto) {
+    const {page, limit} = paginationDto
+    const totalPages = await this.product.count( {where: {available: true}})
+    const lastPage = Math.ceil(totalPages / limit)
+
+    return {
+      data: await this.product.findMany({
+        skip: (page - 1) * limit,
+        take: limit,
+        where: {
+          available: true
+        }
+      }),
+      meta: {
+        totalPages: totalPages,
+        page: page,
+        lastPage: lastPage
+      }
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: number) {
+    const product = await this.product.findFirst({
+      where: {id, available: true}
+    })
+    if (!product){
+      throw new NotFoundException(`Product with id #${id} not found`)
+    }
+
+    return product
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    await this.findOne(id);
+    return this.product.update({
+      where: {id},
+      data: updateProductDto
+    })
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+
+  async remove(id: number) {
+    await this.findOne(id)
+
+    const product = await this.product.update({
+      where: {id},
+      data: {
+        available: false
+      }
+    })
+
+    return product
+
+    //Hard-delete
+    // return this.product.delete({
+    //   where: {id}
+    // })
+
   }
 }
